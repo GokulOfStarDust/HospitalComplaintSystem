b980e1d759bf04e64d0c526c985bfb762b590f5f
GokulofStarDust <gokulgopalan18@gmail.com>
2025-09-20 23:30:41 +0530

    added env variable for dev

--- src/pages/Url.jsx
+++ src/pages/Url.jsx
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://localhost:8000/"
-export const DEPARTMENT_URL = "api/departments/";
-export const ISSUE_CATEGORY_URL = "api/issue-category/";
-export const ROOMS_URL = `api/rooms/`
-export const COMPLAINT_URL = `api/complaints/`
-export const REPORT_URL = `api/report/all_department_stats/`
-export const TICKET_TAT_URL = `api/TATView/all_department_TATS/`
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const DEPARTMENT_URL = import.meta.env.VITE_DEPARTMENT_URL;
+export const ISSUE_CATEGORY_URL = import.meta.env.VITE_ISSUE_CATEGORY_URL;
+export const ROOMS_URL = import.meta.env.VITE_ROOMS_URL;
+export const COMPLAINT_URL = import.meta.env.VITE_COMPLAINT_URL;
+export const REPORT_URL = import.meta.env.VITE_REPORT_URL;
+export const TICKET_TAT_URL = import.meta.env.VITE_TICKET_TAT_URL;
